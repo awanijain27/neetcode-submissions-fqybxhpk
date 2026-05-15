cd76e18656cfb657361d30af63a66de95d4851b6
awanijain27 <113438380+awanijain27@users.noreply.github.com>
2026-05-16 03:22:42 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] arr) {
+        int min=arr[0], p=0;
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]<min) min=arr[i];
+            else p=Math.max(p, arr[i]-min);
+        }
+        return p;
+    }
+}
